64b9ae9e0e02b016a33cccd301a9ee430ec23cdc
loanhjhj <loanb1910094@student.ctu.edu.vn>
2023-04-04 07:36:01 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -41,8 +41,8 @@
                     :to="{
                         name: 'contact.edit',
                         params: { id: activeContact._id },
-                    }"
-                >
+                        }"
+                    >
                     <span class="mt-2 badge badge-warning">
                         <i class="fas fa-edit"></i> Hiệu chỉnh</span
                     >
